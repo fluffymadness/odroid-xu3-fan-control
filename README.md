aafec8a97fafe3ce70c3fd9866d60f9e9a7b2286
fluffymadness <fluffymadness@posteo.de>
2016-01-13 21:31:42 +0100

    Inital Commit

--- README.md
+++ README.md
@@ -1,54 +1,17 @@
-# odroid-xu3-fan-control
-Makes Odroid XU3 Lite / XU4 fan silent on idle load
+Odroid Fan Control
+---
+Makes the XU3 and XU4 a bit less noisy
 
-## Odroid XU3 Lite
+Will work on:
 
-Will only work on:
-* *Ubuntu 14.04.1*, *Ubuntu 15.04*
-* Arch Linux (3.10.69-1-ARCH)
+Lakka
 
-Was not tested anywhere else. Might work though after adjusting script variables
+Precautions
+---
 
-## Precautions
+Use this script on your own risk.
+The original author tested it only on XU3 Lite and I only did a quick test after my modifications.
 
-### *Odroid XU3 Lite* is supported
-
-I only own Odroid XU3 Lite and test this device only
-
-### Odroid XU4
-
-Some users report it works on XU4. *Some users report it doesn't*. Use at your own risk (@see Issues)
-
-### Is it safe to use?
-
-I did my best to adjust fan speed based on current *maximum* temperature of any sensor.
-
-Also, when script quits it brings fan mode back to *automatic* "factory" settings.
-
-I use and will use this software. But in the end - if anything happens to your hardware, do not blame me please.
-
-## Usage
-
-    sudo apt-get install git
-    mkdir -p /home/odroid/
-    cd /home/odroid/ #make sure you can write to this directory; otherwise update paths below
-    git clone https://github.com/nthx/odroid-xu3-fan-control.git
-    cd odroid-xu3-fan-control/
-    sudo ./odroid-xu3-fan-control.sh
-
-## Installation
-
-To make it start when system boots:
-
-    cd /etc/init.d/
-    sudo ln -s /home/odroid/odroid-xu3-fan-control/odroid-fan-controller
-    sudo update-rc.d odroid-fan-controller defaults
-
-To start manually
-
-    sudo /etc/init.d/odroid-fan-controller start
-
-To stop manually
-
-    sudo /etc/init.d/odroid-fan-controller stop
-
+I modified it for XU4 and Lakka and set a minimum fanspeed of 60,
+because the fan of the XU4 gives off a high frequency noise at lower speeds.
+If you change the fan, feel free to modify the speed values according to your wishes ;)
--- odroid-xu3-fan-control.sh
+++ odroid-xu3-fan-control.sh
@@ -6,14 +6,10 @@
 # See README.md for details.
 
 #set to false to suppress logs
+
+
 DEBUG=false
 
-# Make sure only root can run our script
-if (( $EUID != 0 )); then
-   echo "This script must be run as root:" 1>&2
-   echo "sudo $0" 1>&2
-   exit 1
-fi
 
 if [ -f /sys/devices/odroid_fan.13/fan_mode ]; then
    FAN=13
@@ -24,72 +20,86 @@ else
    exit 1
 fi
 
-TEMPERATURE_FILE="/sys/devices/10060000.tmu/temp"
 FAN_MODE_FILE="/sys/devices/odroid_fan.$FAN/fan_mode"
-FAN_SPEED_FILE="/sys/devices/odroid_fan.$FAN/pwm_duty"
-TEST_EVERY=3 #seconds
-LOGGER_NAME=odroid-xu3-fan-control
 
 #make sure after quiting script fan goes to auto control
-function cleanup {
-  ${DEBUG} && logger -t $LOGGER_NAME "event: quit; temp: auto"
-  echo 1 > ${FAN_MODE_FILE}
-}
-trap cleanup EXIT
-
-function exit_xu3_only_supported {
-  ${DEBUG} && logger -t $LOGGER_NAME "event: non-xu3 $1"
-  exit 2
-}
-if [ ! -f $TEMPERATURE_FILE ]; then
-  exit_xu3_only_supported "no temp file"
-elif [ ! -f $FAN_MODE_FILE ]; then
-  exit_xu3_only_supported "no fan mode file"
-elif [ ! -f $FAN_SPEED_FILE ]; then
-  exit_xu3_only_supported "no fan speed file"
+if [ "$1" == "stop" ]
+then
+	echo 1 > $FAN_MODE_FILE
 fi
 
+if [ "$1" == "start" ]
+then
 
-current_max_temp=`cat ${TEMPERATURE_FILE} | cut -d: -f2 | sort -nr | head -1`
-echo "fan control started. Current max temp: ${current_max_temp}"
-echo "For more logs see:"
-echo "sudo tail -f /var/log/syslog"
+	TEMPERATURE_FILE="/sys/devices/10060000.tmu/temp"
+	FAN_SPEED_FILE="/sys/devices/odroid_fan.$FAN/pwm_duty"
+	TEST_EVERY=3 #seconds
+	LOGGER_NAME=odroid-xu3-fan-control
 
-prev_fan_speed=0
-echo 0 > ${FAN_MODE_FILE} #to be sure we can manage fan
+	exit_xu3_only_supported () {
+	  $DEBUG && logger -t $LOGGER_NAME "event: non-xu3 $1"
+	  exit 2
+	}
+	if [ ! -f $TEMPERATURE_FILE ]; then
+	  exit_xu3_only_supported "no temp file"
+	elif [ ! -f $FAN_MODE_FILE ]; then
+	  exit_xu3_only_supported "no fan mode file"
+	elif [ ! -f $FAN_SPEED_FILE ]; then
+	  exit_xu3_only_supported "no fan speed file"
+	fi
 
-while [ true ];
-do
 
-  current_max_temp=`cat ${TEMPERATURE_FILE} | cut -d: -f2 | sort -nr | head -1`
-  ${DEBUG} && logger -t $LOGGER_NAME "event: read_max; temp: ${current_max_temp}"
+	current_max_temp=`cat $TEMPERATURE_FILE | cut -d: -f2 | sort -nr | head -1`
+	echo "fan control started. Current max temp: $current_max_temp"
+	echo "For more logs see:"
+	echo "sudo tail -f /var/log/syslog"
 
-  new_fan_speed=0
-  if (( ${current_max_temp} >= 75000 )); then
-    new_fan_speed=255
-  elif (( ${current_max_temp} >= 70000 )); then
-    new_fan_speed=200
-  elif (( ${current_max_temp} >= 68000 )); then
-    new_fan_speed=130
-  elif (( ${current_max_temp} >= 66000 )); then
-    new_fan_speed=70
-  elif (( ${current_max_temp} >= 63000 )); then
-    new_fan_speed=65
-  elif (( ${current_max_temp} >= 60000 )); then
-    new_fan_speed=60
-  elif (( ${current_max_temp} >= 58000 )); then
-    new_fan_speed=50
-  elif (( ${current_max_temp} >= 55000 )); then
-    new_fan_speed=30
-  else
-    new_fan_speed=2
-  fi
+	prev_fan_speed=0
+	echo 0 > $FAN_MODE_FILE #to be sure we can manage fan
+	while [ true ];
+	do
 
-  if (( ${prev_fan_speed} != ${new_fan_speed} )); then
-    ${DEBUG} && logger -t $LOGGER_NAME "event: adjust; speed: ${new_fan_speed}"
-    echo ${new_fan_speed} > ${FAN_SPEED_FILE}
-    prev_fan_speed=${new_fan_speed}
-  fi
+	  current_max_temp=`cat $TEMPERATURE_FILE | cut -d: -f2 | sort -nr | head -1`
+	  $DEBUG && logger -t $LOGGER_NAME "event: read_max; temp: $current_max_temp"
+	  #echo $current_max_temp
 
-  sleep ${TEST_EVERY}
-done
+	  new_fan_speed=0
+	  if [ $current_max_temp -ge 75000 ]
+	  then
+		new_fan_speed=255
+	  elif [ $current_max_temp -ge 70000 ]
+	  then
+		new_fan_speed=200
+	  elif [ $current_max_temp -ge 68000 ]
+	  then
+		new_fan_speed=130
+	  elif [ $current_max_temp -ge 66000 ]
+	  then
+		new_fan_speed=70
+	  elif [ $current_max_temp -ge 63000 ]
+	  then
+		new_fan_speed=70
+	  elif [ $current_max_temp -ge 60000 ]
+	  then
+		new_fan_speed=70
+	  elif [ $current_max_temp -ge 58000 ]
+	  then
+		new_fan_speed=70
+	  elif [ $current_max_temp -ge 55000 ]
+	  then
+		new_fan_speed=70
+	  else
+		#0
+		new_fan_speed=70
+	  fi
+
+	  if [ $prev_fan_speed -ne $new_fan_speed ]
+	  then
+		$DEBUG && logger -t $LOGGER_NAME "event: adjust; speed: ${new_fan_speed}"
+		echo $new_fan_speed > $FAN_SPEED_FILE
+		prev_fan_speed=$new_fan_speed
+	  fi
+
+	  sleep $TEST_EVERY
+	done
+fi
